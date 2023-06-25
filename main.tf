2ef3e9d478b01814fb0e53d8aaae0f927636169b
vagrant <vagrant@dev-node040>
2023-06-25 18:39:59 +0000

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "eks-module" {
+  source         = "./modules/"
+  region         = "us-east-1"
+  vpc_cidr       = "10.0.0.0/16"
+  dns_hostnames  = true
+  dns_support    = true
+  pub_one_cidr   = "10.0.1.0/24"
+  pub_two_cidr   = "10.0.2.0/24"
+  priv_one_cidr  = "10.0.3.0/24"
+  priv_two_cidr  = "10.0.4.0/24"
+  az_one         = "us-east-1a"
+  az_two         = "us-east-2b"
+  vpc_id         = "aws_vpc.eks_vpc.id"
+  eks_version    = "1.26"
+  ami_type       = "AL2_x86_64"
+  instance_types = ["t3.small", "t3.medium", "t3.large"]
+  capacity_type  = "ON_DEMAND"
+}
